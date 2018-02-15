26cc7f74e543fe0bc00de948eb7875b8de8bd20d
Tim Anderson-Bonsor <tim@thecanarstudio.com>
2018-02-15 14:59:28 +1100

    Drop in v9 base scss

--- resources/assets/scripts/main.js
+++ resources/assets/scripts/main.js
@@ -2,21 +2,7 @@
 import 'jquery';
 
 // Import everything from autoload
-// import "./autoload/**/*"
-
-// Import Bootstrap selected componenets
-// import 'bootstrap/js/src/alert.js';
-// import 'bootstrap/js/src/button.js';
-// import 'bootstrap/js/src/carousel.js';
-import 'bootstrap/js/src/collapse.js';
-import 'bootstrap/js/src/dropdown.js';
-// import 'bootstrap/js/src/index.js';
-// import 'bootstrap/js/src/modal.js';
-// import 'bootstrap/js/src/popover.js';
-// import 'bootstrap/js/src/scrollspy.js';
-// import 'bootstrap/js/src/tab.js';
-// import 'bootstrap/js/src/tooltip.js';
-import 'bootstrap/js/src/util.js';
+import "./autoload/**/*"
 
 // import local dependencies
 import Router from './util/Router';
