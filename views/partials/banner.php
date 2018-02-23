1b2d8c08eaf3777086b087af9b19f4ae2b7cc25e
Tim Anderson-Bonsor <tim@thecanarstudio.com>
2018-02-23 22:17:13 +1100

    Add banner example with SVG

--- views/partials/banner.php
+++ views/partials/banner.php
@@ -1,10 +1,13 @@
 <div class="banner-primary pb-5 pt-5 mb-5">
   <div class="container">
-    <h1 class="display-3">Hello, world!</h1>
-    <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and
-      three supporting pieces of content. Use it as a starting point to create something more unique.</p>
+    <h1 class="display-1 icon-white"> 
+      <?php get_template_part( 'resources/assets/images/inline', 'test.svg' ); ?>
+      Canary
+    </h1>
+    <p class="lead mr-5 pr-5">This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and
+      three supporting pieces of content. Use it as a starting point to create something more unique. </p>
     <p>
-      <a class="btn btn-primary btn-lg" href="#" role="button">Learn more »</a>
+      <a class="btn btn-primary btn-lg" href="#" role="button"><span class="icon-orange"><?php get_template_part('resources/assets/images/inline', 'test.svg');?></span>Learn more </a>
     </p>
   </div>
 </div>
