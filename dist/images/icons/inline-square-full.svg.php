182fc808e29fec8b085abf48ace080920e887ffa
Tim Anderson-Bonsor <tim@thecanarstudio.com>
2018-03-16 00:14:15 +1100

    Add dist

---
+++ dist/images/icons/inline-square-full.svg.php
@@ -0,0 +1,1 @@
+<svg aria-hidden="true" data-prefix="fal" data-icon="square-full" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="svg-inline--fa fa-square-full fa-w-16" font-size="48"><path d="M480 32v448H32V32h448m32-32H0v512h512V0z"/></svg>
