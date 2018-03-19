38064025aef6a0724e19252074062405ac99c4d5
Tim Anderson-Bonsor <tim@thecanarstudio.com>
2018-03-20 00:27:20 +1100

    Add Bootstrap carousel js

--- resources/assets/scripts/main.js
+++ resources/assets/scripts/main.js
@@ -13,10 +13,10 @@ import 'jquery';
 import 'bootstrap/js/src/collapse.js';
 import 'bootstrap/js/src/dropdown.js';
 // import 'bootstrap/js/src/index.js';
-// import 'bootstrap/js/src/modal.js';
+import 'bootstrap/js/src/modal.js';
 // import 'bootstrap/js/src/popover.js';
-// import 'bootstrap/js/src/scrollspy.js';
-// import 'bootstrap/js/src/tab.js';
+import 'bootstrap/js/src/scrollspy.js';
+import 'bootstrap/js/src/tab.js';
 // import 'bootstrap/js/src/tooltip.js';
 // import 'bootstrap/js/src/util.js';
 
