d2088db7cada4b01236b53fec026d1ff93125055
Tim Anderson-Bonsor <tim@thecanarstudio.com>
2018-08-18 20:11:21 +1000

    Add speaker content partial

---
+++ views/partials/content-speaker.php
@@ -0,0 +1,13 @@
+<div class="speaker-block col-sm-4 d-flex">
+  <div class="bg-white mb-3 position-relative">
+    <img src="http://placehold.it/400x250" class="img-fluid">
+    <div class="p-3 mr-auto">
+      <p class="lead">
+        Hari Krishnan
+        <br> Senior Director, Product Management and Security
+        <br> Nokia
+      </p>
+    </div>
+    <a href="" data-toggle="modal" data-target="#exampleModal" class="btn-icon btn-enlarge d-block">Enlarge</a>
+  </div>
+</div>
