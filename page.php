6d58be0520d563ef357a259c0947cfe580453127
Tim Anderson-Bonsor <tim@thecanarstudio.com>
2018-09-27 23:14:16 +1000

    Update container overflow

--- page.php
+++ page.php
@@ -1,4 +1,9 @@
+
+
 <?php while (have_posts()) : the_post(); ?>
-  <?php get_template_part('views/partials/page', 'header'); ?>
+
+<?php get_template_part('views/partials/page-header-small'); ?>
+<section class="content-row py-7"><
   <?php get_template_part('views/partials/content', 'page'); ?>
+</section>
 <?php endwhile; ?>
