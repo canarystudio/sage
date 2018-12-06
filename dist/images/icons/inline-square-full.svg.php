e6ceeca5b28d7cf68f271414d5ea9cb47c41a563
Tim Anderson-Bonsor <tim@thecanarstudio.com>
2018-12-06 21:32:45 +1100

    Update svg icons

--- dist/images/icons/inline-square-full.svg.php
+++ dist/images/icons/inline-square-full.svg.php
@@ -1,1 +1,1 @@
-<svg aria-hidden="true" data-prefix="fal" data-icon="square-full" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="svg-inline--fa fa-square-full fa-w-16" font-size="48"><path d="M480 32v448H32V32h448m32-32H0v512h512V0z"/></svg>
+<svg aria-hidden="true" data-prefix="fal" data-icon="square-full" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="svg-inline--fa fa-square-full fa-w-16" style="font-size: 48px;"><path fill="#000000" d="M480 32v448H32V32h448m32-32H0v512h512V0z" class="" stroke="none" stroke-width="1px"></path></svg>
