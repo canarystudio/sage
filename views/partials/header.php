fdc2e2dcc7d481406e54c8b03548675639fe63f1
Tim Anderson-Bonsor <tim@thecanarstudio.com>
2018-12-18 17:34:14 +1100

    Code tidy for header options

--- views/partials/header.php
+++ views/partials/header.php
@@ -1,5 +1,47 @@
-<nav class="navbar navbar-expand-lg  navbar-dark bg-primary">
-  <a class="navbar-brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
+<?php
+  /**
+   * Setup header content and options
+   * Pull in ACF fields from optoins page to set styling options
+   */
+
+  // get nav placement option field
+  if (get_field('event_navigation_placement', 'option')) {
+    $event_navigation_placement = get_field('event_navigation_placement', 'option');
+
+  } else {
+    $event_navigation_placement = "";
+  };
+
+
+  // get nav color option field
+  if (get_field('event_navigation_placement', 'option')) {
+    $event_navigation_colour_field = get_field('event_navigation_colour', 'option');
+
+    if ( $event_navigation_colour_field == 'dark' ) {
+      $event_navigation_colour = 'navbar-dark bg-primary';
+    } else {
+      $event_navigation_colour = 'navbar-light bg-white';
+    }
+  } else {
+    $event_navigation_colour = 'navbar-dark bg-primary';
+  };
+
+
+  // get logo
+  if ( get_field('event_logo', 'option') ) { // if image is available
+    $event_logo = get_field('event_logo', 'option');
+
+  } elseif ( get_field('event_name', 'option') ) { // fall back to ACF option name field
+    $event_logo = get_field('event_name', 'option');
+
+  } else { // fall back to WP blog title
+    $event_logo = get_bloginfo('name');
+  };
+
+
+?>
+<nav class="navbar navbar-expand-lg <?php echo $event_navigation_colour; ?> <?php echo $event_navigation_placement; ?>">
+  <a class="navbar-brand" href="<?= esc_url(home_url('/')); ?>"><?php echo $event_logo; ?></a>
   <button class="hamburger hamburger--spin navbar-expand-md navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
   <span class="hamburger-box">
     <span class="hamburger-inner"></span>
@@ -20,7 +62,7 @@
           'container'			=> 'div',
           'container_class'   => 'collapse navbar-collapse',
           'container_id'      => 'bs-example-navbar-collapse-1',
-          'menu_class'        => 'navbar-nav mr-auto mt-2 mt-lg-0',
+          'menu_class'        => 'navbar-nav ml-auto mt-lg-0',
           'fallback_cb'		=> 'WP_Bootstrap_Navwalker::fallback',
           'walker'			=> new WP_Bootstrap_Navwalker()
         ]);
