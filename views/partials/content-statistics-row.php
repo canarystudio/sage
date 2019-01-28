e7b15990f14eeb9f4d6214d1fdd14e1706ee2095
Tim Anderson-Bonsor <tim@bisoncreative.com.au>
2019-01-29 00:19:04 +1100

    Fix text sizes on stats

--- views/partials/content-statistics-row.php
+++ views/partials/content-statistics-row.php
@@ -3,7 +3,7 @@
   if( get_row_layout() == 'statistics_row' ) {
 ?>
 
-<section class="content-row text-center py-7 full-width
+<section class="stastics-row content-row text-center py-7 full-width
   <?php if ( get_sub_field('statistics_row_background') == 'primary' ) { ?>
   bg-primary text-light
   <?php $large_text_color = 'text-secondary'; ?>
@@ -27,7 +27,7 @@
   <div class="row justify-content-center mb-3">
     <?php while ( have_rows('stats') ) : the_row(); ?>
     <p class="col-sm-2">
-      <span class="h1 display-1 <?php echo $large_text_color; ?>"><?php the_sub_field('number'); ?></span><br>
+      <span class="h1 display-4 <?php echo $large_text_color; ?>"><?php the_sub_field('number'); ?></span><br>
       <?php the_sub_field('label'); ?>
     </p>
     <?php endwhile; ?>
