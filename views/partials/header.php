1fb4d4251814756c924ad1b90281c29782dc09e8
Tim Anderson-Bonsor <tim@bisoncreative.com.au>
2019-01-30 13:51:29 +1100

    Fix header branding

--- views/partials/header.php
+++ views/partials/header.php
@@ -40,36 +40,52 @@
 
 
 ?>
-<nav class="navbar navbar-expand-lg <?php echo $event_navigation_colour; ?> <?php echo $event_navigation_placement; ?>">
-  <a class="navbar-brand" href="<?= esc_url(home_url('/')); ?>"><?php echo $event_logo; ?></a>
-  <button class="hamburger hamburger--spin navbar-expand-md navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-  <span class="hamburger-box">
-    <span class="hamburger-inner"></span>
-  </span>
-</button>
-  <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-    <?php
+<header class="header-primary">
+  <nav class="navbar navbar-expand-lg  px-6 <?php echo $event_navigation_colour; ?> <?php echo $event_navigation_placement; ?>">
+    <a class="navbar-brand" href="<?= esc_url(home_url('/')); ?>">
+      <?php echo $event_logo; ?>
+    </a>
+    <button class="hamburger hamburger--spin navbar-expand-md navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="hamburger-box">
+        <span class="hamburger-inner"></span>
+      </span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+      <?php
+      /**
+       * Detect plugin. For use on Front End and Back End.
+       */
+      // check for plugin using plugin name
+      if(in_array('canary-wp-boostrap4-navwalker/canary-wp-boostrap4-navwalker.php', apply_filters('active_plugins', get_option('active_plugins')))){
+        if (has_nav_menu('primary_navigation')) :
+          wp_nav_menu([
+            'menu'              => 'primary_navigation',
+            'theme_location'    => 'primary_navigation',
+            'depth'       => 2, // 1 = with dropdowns, 0 = no dropdowns.
+            'container'     => 'div',
+            'container_class'   => 'collapse navbar-collapse',
+            'container_id'      => 'bs-example-navbar-collapse-1',
+            'menu_class'        => 'navbar-nav ml-auto mt-lg-0',
+            'fallback_cb'   => 'WP_Bootstrap_Navwalker::fallback',
+            'walker'      => new WP_Bootstrap_Navwalker()
+          ]);
+        endif;
+      } else {
+          echo "<script>console.log('Enable the navwalker plugin to display a menu.');</script>";
+      }
+      ?>
+    </div>
+  </nav>
+
+  <?php
     /**
-     * Detect plugin. For use on Front End and Back End.
+     * Check for header type
+     * Use the small header for all banners other than front page
      */
-    // check for plugin using plugin name
-    if(in_array('canary-wp-boostrap4-navwalker/canary-wp-boostrap4-navwalker.php', apply_filters('active_plugins', get_option('active_plugins')))){
-      if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu([
-          'menu'              => 'primary_navigation',
-          'theme_location'    => 'primary_navigation',
-          'depth'				=> 2, // 1 = with dropdowns, 0 = no dropdowns.
-          'container'			=> 'div',
-          'container_class'   => 'collapse navbar-collapse',
-          'container_id'      => 'bs-example-navbar-collapse-1',
-          'menu_class'        => 'navbar-nav ml-auto mt-lg-0',
-          'fallback_cb'		=> 'WP_Bootstrap_Navwalker::fallback',
-          'walker'			=> new WP_Bootstrap_Navwalker()
-        ]);
-      endif;
+    if ( is_front_page() ) {
+      get_template_part('views/partials/page-header', 'large');
     } else {
-        echo "<script>console.log('Enable the navwalker plugin to display a menu.');</script>";
-    }
-    ?>
-  </div>
-</nav>
+      get_template_part('views/partials/page-header', 'small');
+    };
+  ?>
+</header>
