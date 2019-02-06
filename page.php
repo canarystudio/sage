6b531e29d66492f966ca95b5651462d9f75049a0
Tim Anderson-Bonsor <tim@bisoncreative.com.au>
2019-02-06 15:48:44 +1100

    Make page content conditional for front page

--- page.php
+++ page.php
@@ -1,4 +1,15 @@
-<?php while (have_posts()) : the_post(); ?>
-  <?php get_template_part('views/partials/page-header'); ?>
-  <?php get_template_part('views/partials/content', 'page'); ?>
-<?php endwhile; ?>
+<?php
+while (have_posts()) : the_post();
+
+  // get page header
+  get_template_part('views/partials/page-header');
+
+  // get page content, check homepage or inner page
+  if ( is_front_page() ) {
+    get_template_part('views/partials/content', 'home');
+  } else {
+    get_template_part('views/partials/content', 'page');
+  };
+
+endwhile;
+?>
