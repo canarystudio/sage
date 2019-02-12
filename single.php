8abdb3da9630192bfa451eb01cd792d23511d086
Tim Anderson-Bonsor <tim@andersonbonsor.com>
2019-02-13 00:23:22 +1100

    Make header call dynamic in single

--- single.php
+++ single.php
@@ -1,5 +1,5 @@
 
-<?php get_template_part('views/partials/page-header-small'); ?>
+<?php get_template_part('views/partials/page-header-small', get_post_type()); ?>
 <section class="content-row py-7">
   <?php get_template_part('views/partials/content-single', get_post_type()); ?>
 </section>
