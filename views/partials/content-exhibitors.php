23301fcd7b80b565d00dc41ba7add39cf20c47d5
Tim Anderson-Bonsor <tim@andersonbonsor.com>
2019-02-21 11:38:18 +1100

    Update exhibitor pages for mobile

--- views/partials/content-exhibitors.php
+++ views/partials/content-exhibitors.php
@@ -1,13 +1,13 @@
 <div class="row">
-  <div class="col-sm-3 pr-2">
-    <div><?php the_post_thumbnail( 'thumbnail', array( 'class' => 'img-fluid' ) ); ?></div>
+  <div class="col-sm-3 mb-1 mb-sm-0 pr-sm-2">
+    <div><a href="<?php the_permalink(); ?>"><?php the_post_thumbnail( 'thumbnail', array( 'class' => 'img-fluid' ) ); ?></a></div>
   </div>
   <div class="col-sm-9 exhibitor-item-content">
     <h4 class="m-0 exhibitor-title"><a href="<?php the_permalink(); ?>"><?php echo the_title(); ?></a></h4>
     <p>
       <?php if( get_field('representative') ): ?>Representative: <strong><?php the_field('representative'); ?></strong><?php endif; ?>
       <?php if( get_field('email_contact_x') ): ?><br>Contact: <?php the_field('email_contact'); ?><?php endif; ?>
-      <?php if( get_field('booth_number') ): ?><br>Booth <?php the_field('booth_number'); ?><?php endif; ?>
+      <?php if( get_field('booth_number') ): ?><br>Booth Number: <?php the_field('booth_number'); ?><?php endif; ?>
       <?php if( get_field('website_address') ): ?><br>Website: <a href="<?php the_field('website_address'); ?>"><?php the_field('website_address'); ?></a><?php endif; ?>
     </p>
   </div>
--- views/partials/content-single-exhibitors.php
+++ views/partials/content-single-exhibitors.php
@@ -1,36 +1,33 @@
 <?php while (have_posts()) : the_post(); ?>
-<p class="px-8 mt-n3 mb-4 lead text-right"><a href="/exhibitors/" class="text-secondary"><strong>← All Exhibitors</strong></a></p>
-  <article <?php post_class('px-8 dragon-head dragon-head-right'); ?>>
 
+<p class="px-4 px-sm-8 mt-n4 mb-sm-4 text-right"><a href="/exhibitors/" class="text-secondary"><strong>← All Exhibitors</strong></a></p>
+
+<article <?php post_class('px-4 px-sm-8'); ?>>
 
   <section class="row mb-8">
+    <div class="col-sm-8 order-2 order-sm-1 exhibitor-item-content">
+      <h1 class="m-0 exhibitor-title letter-spacing-normal"><?php echo the_title(); ?></h1>
+      <p class="lead">
+        <?php if( get_field('representative') ): ?><span class="d-flex flex-column flex-sm-row pb-1 pb-sm-0 align-items-sm-center"><small class="pr-1">Representative:</small> <strong><?php the_field('representative'); ?></strong></span><?php endif; ?>
+        <?php if( get_field('email_contact_x') ): ?><span class="d-flex flex-column flex-sm-row pb-1 pb-sm-0 align-items-sm-center"><small class="pr-1">Contact:</small> <strong><?php the_field('email_contact'); ?></strong></span><?php endif; ?>
+        <?php if( get_field('website_address') ): ?><span class="d-flex flex-column flex-sm-row pb-1 pb-sm-0 align-items-sm-center"><small class="pr-1">Website:</small> <strong><a href="<?php the_field('website_address'); ?>"><?php the_field('website_address'); ?></a></strong></span><?php endif; ?>
+        <?php if( get_field('booth_number') ): ?><span class="d-flex flex-column flex-sm-row pb-1 pb-sm-0 align-items-sm-center"><small class="pr-1">Booth:</small> <strong><a href="/exhibiting/#floorplan" class="text-primary"><?php the_field('booth_number'); ?></a></strong></span><?php endif; ?>
+      </p>
+      <p class="mb-4"><a href="#appointment" class="smooth btn btn-sm btn-dark"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-message-square"><path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path></svg> Request an appointment</a></p>
+      <div class="pr-4"><?php the_content(); ?></div>
+    </div>
+    <div class="col-sm-4 pl-sm-2 mb-3 mb-sm-0 order-1 order-sm-2">
+      <div><?php the_post_thumbnail( 'thumbnail', array( 'class' => 'img-fluid' ) ); ?></div>
+    </div>
+  </section>
 
-  <div class="col-sm-8 exhibitor-item-content">
-    <h1 class="m-0 exhibitor-title display-1"><?php echo the_title(); ?></h1>
-    <p class="lead">
-      <?php if( get_field('representative') ): ?>Representative: <strong><?php the_field('representative'); ?></strong><?php endif; ?>
-      <?php if( get_field('email_contact_x') ): ?><br>Contact: <?php the_field('email_contact'); ?><?php endif; ?>
-      <?php if( get_field('booth_number') ): ?><br>Booth: <?php the_field('booth_number'); ?> <a href="" class="text-dark small pl-2"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-external-link"><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path><polyline points="15 3 21 3 21 9"></polyline><line x1="10" y1="14" x2="21" y2="3"></line></svg> view floorplan</a><?php endif; ?>
-      <?php if( get_field('website_address') ): ?><br>Website: <a href="<?php the_field('website_address'); ?>"><?php the_field('website_address'); ?></a><?php endif; ?>
-    </p>
-    <p class="mb-4"><a href="#appointment" class="smooth btn btn-sm btn-dark"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-message-square"><path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path></svg> Request an appointment</a></p>
-
-    <div class="pr-4"><?php the_content(); ?></div>
+  <div class="pr-sm-16" id="appointment">
+    <h3 class="letter-spacing-normal">Request an Appointment</h3>
+    <p><?php the_title(); ?> will be available on the exhibition floor for meetings during the Exhibition Open hours.</p>
+    <p>Please nominate if you would prefer a morning or afternoon meeting time below – a representative from <?php the_title(); ?> will be in direct contact with you via the email you enter below to confirm the meeting time.</p>
+    <?php gravity_form(10, false, false, false, '', true); ?>
   </div>
-  <div class="col-sm-4 pl-2">
-    <div><?php the_post_thumbnail( 'thumbnail', array( 'class' => 'img-fluid' ) ); ?></div>
-  </div>
-</section>
 
+</article>
 
-
-
-      <div class="mw-col-8" id="appointment">
-        <h2 class="letter-spacing-normal"><small>Request an Appointment with</small><br><span class="display-2"><?php the_title(); ?></span></h2>
-        <p><?php the_title(); ?> will be available on the exhibition floor for meetings during the Exhibition Open hours.</p>
-        <p>Please nominate if you would prefer a morning or afternoon meeting time below – a representative from <?php the_title(); ?> will be in direct contact with you via the email you enter below to confirm the meeting time.</p>
-        <?php gravity_form(10, false, false, false, '', true); ?>
-      </div>
-
-    </article>
 <?php endwhile; ?>
