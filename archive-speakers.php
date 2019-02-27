09b17d0c8e177688f6f7b0568e2b01cbfc7cdba2
Tim Anderson-Bonsor <tim@andersonbonsor.com>
2019-02-27 15:57:51 +1100

    Make speaker thumbs in a standard grid

--- archive-speakers.php
+++ archive-speakers.php
@@ -7,9 +7,11 @@
   </div>
   <?php get_search_form(); ?>
 <?php endif; ?>
-<div class="card-columns">
+<div class="row row-speakers mx-sm-n1">
   <?php while (have_posts()) : the_post(); ?>
-  <?php get_template_part('views/partials/content', get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
+  <div class="col-sm-3 px-sm-1">
+    <?php get_template_part('views/partials/content', get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
+  </div>
   <?php endwhile; ?>
 </div>
 <?php the_posts_navigation(); ?>
