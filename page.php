d0b801a5e4f1fd8a70d50999d790418e485e2123
Tim Anderson-Bonsor <tim@andersonbonsor.com>
2019-04-19 01:19:36 +1000

    Remove header double-up

--- page.php
+++ page.php
@@ -1,15 +1,7 @@
 <?php
 while (have_posts()) : the_post();
 
-  // get page header
-  get_template_part('views/partials/page-header');
-
-  // get page content, check homepage or inner page
-  if ( is_front_page() ) {
-    get_template_part('views/partials/content', 'home');
-  } else {
-    get_template_part('views/partials/content', 'page');
-  };
+  get_template_part('views/partials/content', 'page');
 
 endwhile;
 ?>
