fd462a7a1e5e2844376347064b8a41d97823d18d
Tim Anderson-Bonsor <tim@andersonbonsor.com>
2019-05-03 22:20:16 +1000

    Make exhibitor heading white

--- views/partials/content-single-exhibitors.php
+++ views/partials/content-single-exhibitors.php
@@ -6,7 +6,7 @@
 
   <section class="row mb-8">
     <div class="col-sm-8 order-2 order-sm-1 exhibitor-item-content">
-      <h1 class="m-0 exhibitor-title letter-spacing-normal text-white"><?php echo the_title(); ?></h1>
+      <h1 class="m-0 exhibitor-title letter-spacing-normal"><?php echo the_title(); ?></h1>
       <p class="lead">
         <?php if( get_field('representative') ): ?><span class="d-flex flex-column flex-sm-row pb-1 pb-sm-0 align-items-sm-center"><small class="pr-1">Representative:</small> <strong><?php the_field('representative'); ?></strong></span><?php endif; ?>
         <?php if( get_field('email_contact_x') ): ?><span class="d-flex flex-column flex-sm-row pb-1 pb-sm-0 align-items-sm-center"><small class="pr-1">Contact:</small> <strong><?php the_field('email_contact'); ?></strong></span><?php endif; ?>
