a64a2aac38c06037b6eaa648d84f64ee25862f90
Tim Anderson-Bonsor <tim@andersonbonsor.com>
2020-01-15 10:55:03 +1100

    Remove speaker template files from base theme

--- views/partials/content-speakers/speaker-bio.php
+++
@@ -1,3 +0,0 @@
-<?php if( get_field('speaker_bio', $speaker) ): ?>
-  <p><?php the_field('speaker_bio', $speaker); ?></p>
-<?php endif; ?>
--- views/partials/page-header-speakers.php
+++
@@ -1,5 +0,0 @@
-<?php use Roots\Sage\Titles; ?>
-
-<div class="page-header mb-4">
-  <h1>Speakers</h1>
-</div>
